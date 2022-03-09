7eda1c89df695fd4c6346dc3f8b9a1de1fff72c4
Matin G. Aghayev <matinga@code.local>
2022-03-09 06:24:53 -0800

    about

---
+++ Project/Js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        arrows: true,
+        autoplaySpeed:600
+        
+    });
+}); 
